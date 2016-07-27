606795723f31ec75bb6b954964805b6aecb2cf1c
akihiro-nakashima <akihiro.nakashima@nts.ricoh.co.jp>
2016-07-27 16:56:34 +0900

    マスター作成

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hello = new Hello();
+            hello.hellotest();
+        }
+
+      
+    }
+
+    public class Hello
+    {
+        public void hellotest()
+        {
+            Console.WriteLine("helloworld!");
+        }
+
+    }
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1Tests/HelloTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class HelloTests
+    {
+        Hello hello=new Hello();
+        [TestMethod()]
+        public void hellotestTest()
+        {
+            try
+            {
+                hello.hellotest();
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
